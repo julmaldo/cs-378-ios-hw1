c5e44e4b890566fa0f73d07abeb4a16230b13c3c
Julio A. Maldonado <julio.a.maldo@gmail.com>
2015-09-02 20:06:48 -0500

    Initial Commit

---
+++ MaldonadoJulio-hw1/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  MaldonadoJulio-hw1
+//
+//  Created by Julio Maldonado on 9/2/15.
+//  Copyright (c) 2015 Julio Maldonado. All rights reserved.
+//
+
+import Foundation
+
+println("Hello, World!")
+
